e73f403e09c8fdab0186c7f3ac49988e2c8fcaad
Sachintha Madushan Rathnayake <madushansachintha@gmail.com>
2019-04-04 08:12:28 +0530

    Added michelle's code to the application

--- src/templateMatcherV1.0.py
+++ src/templateMatcherV1.0.py
@@ -68,48 +68,37 @@ def setTemplate():
 
 def proceed():
     if(parentImage and templateImage):
-        #import parent image and template image
-        parent_image=parentImage
-        template_image=templateImage
-
-        # Read the parent image 
-        mask = cv2.imread(parent_image)
-        img_parent = cv2.imread(parent_image)
-        img = cv2.imread(parent_image,0)
-
-        # Convert parent image to grayscale 
-        img_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY) 
-
-        # Read the template image
-        template = cv2.imread(template_image,0) 
-
-        # Store width and heigth of template in w and h 
-        w, h = template.shape[::-1]
-
-        # Store width and heigth of parent image in W and H 
-        W, H = img.shape[::-1]
-
-        # Perform match operations. 
-        res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED) 
-
-        # Specify a threshold (to maintain accuracy level)
-        threshold = (top.slider.get())/100
-
-        # Store the coordinates of matched area in a numpy array 
-        cordinates = np.where( res >= threshold)
-
-        # filled mask with black color 
-        cv2.rectangle(mask, (0,0), (W, H), (0,0,0),-1)
-
-        # Draw detected areas on mask as white boxes. 
-        for pt in zip(*cordinates[::-1]):
-            cv2.rectangle(mask, pt, (pt[0] + w, pt[1] + h), (255,255,255),-1) 
-
-        # Merge parent image with mask to provide the output.
-        img_parent = cv2.bitwise_and(img_parent, mask)
-
-        # Show the final image with the matched area.
-        cv2.imshow('Detected',img_parent)
+        #reading images
+        imageGrey=cv2.imread(parentImage,0)
+        templateGrey=cv2.imread(templateImage,0)
+         
+        #template matching
+        result=cv2.matchTemplate(imageGrey,templateGrey,cv2.TM_SQDIFF_NORMED)
+         
+        #finding the occurences of the template
+        #print(top.slider.get())
+        threshold=top.slider.get()
+        templateHeight,templateWidth=templateGrey.shape
+         
+        #checking the normalized squared difference value is smaller than threshold value so thats its closer to the template or equal to the template
+        location=np.where(result <= threshold)
+         
+        #creating black mask for the size of the image
+        mask=np.zeros(imageGrey.shape,dtype=np.uint8)
+         
+         
+        #drawing the rectangle on the mask
+        for point in zip(*location[::-1]):
+            cv2.rectangle(mask, point,(point[0]+templateWidth,point[1]+templateHeight),255,-1)
+         
+        #applying the mask ontop of the image
+        finalimage=cv2.bitwise_and(imageGrey,imageGrey,mask=mask)
+         
+        #Displaying the image
+         
+        #cv2.imshow("Template",templateGrey)
+        cv2.imshow("Template Detected",finalimage)
+        cv2.waitKey(0)
     else:
         messagebox.showerror("Error", "Please Select Images to Proceed")
         
@@ -236,7 +225,7 @@ class Toplevel1:
         self.Template.configure(selectforeground="black")
         self.Template.configure(width=303)
 
-        self.slider = tk.Scale(self.Canvas4, from_=0.0, to=100.0)
+        self.slider = tk.Scale(self.Canvas4, from_=0.0, to=0.1,resolution=0.0001)
         self.slider.place(relx=0.031, rely=0.794, relwidth=0.935, relheight=0.0
                 , height=42, bordermode='ignore')
         self.slider.configure(activebackground="#ececec")
